54271a8ccefa4cb19ad63951726cdcebf22316f0
Gopal Tiwari <gopaltiwariorange@gmail.com>
2026-04-29 22:32:51 +0530

    delete functionality implemented

--- controllers/empSectionContoller/employee.controller.js
+++ controllers/empSectionContoller/employee.controller.js
@@ -1,7 +1,8 @@
 import asyncHandler from "express-async-handler";
 import Employee from "../../models/empSectionModel/employee.model.js";
 export const createEmployee = asyncHandler(async (req, res) => {
-  const { name, email, phone, password, department, designation, role } = req.body;
+  const { name, email, phone, password, department, designation, role } =
+    req.body;
 
   if (
     !name?.trim() ||
@@ -11,51 +12,49 @@ export const createEmployee = asyncHandler(async (req, res) => {
     !department ||
     !designation
   ) {
-
     return res.status(400).json({
       success: false,
-      message: "All fields are required"
+      message: "All fields are required",
     });
   }
 
   const newEmp = await Employee.create({
-    ...req.body
+    ...req.body,
   });
-
 
   res.status(201).json({
     success: true,
-    data: newEmp
+    data: newEmp,
   });
 });
 
-
 export const getAllEmp = asyncHandler(async (req, res) => {
-    const emp = await Employee.find().populate("department", "name").populate("designation", "title");
-    res.status(200).json(emp);
-})
-
+  const emp = await Employee.find({ isDeleted: { $ne: true } })
+    .populate("department", "name")
+    .populate("designation", "title");
+  res.status(200).json(emp);
+});
 
 export const deleteEmployee = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const deleteEmp = await Employee.findByIdAndDelete(id);
-    res.status(200).json(deleteEmp);
-})
+  const { id } = req.params;
+  const deleteEmp = await Employee.findByIdAndUpdate({
+    _id: id,
+    isDeleted: true,
+  });
+  res.status(200).json(deleteEmp);
+});
 
 export const updateEmployee = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const updateData = { ...req.body };
-    
-    // Don't update password if it's empty
-    if (!updateData.password || updateData.password.trim() === "") {
-        delete updateData.password;
-    }
+  const { id } = req.params;
+  const updateData = { ...req.body };
 
-    const updateEmp = await Employee.findByIdAndUpdate(id, updateData, { new: true });
-    res.status(200).json(updateEmp);
-})
+  // Don't update password if it's empty
+  if (!updateData.password || updateData.password.trim() === "") {
+    delete updateData.password;
+  }
 
-
-
-
-
+  const updateEmp = await Employee.findByIdAndUpdate(id, updateData, {
+    new: true,
+  });
+  res.status(200).json(updateEmp);
+});
